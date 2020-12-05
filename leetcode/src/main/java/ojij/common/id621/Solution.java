b0a6c9aa2ee9868fa16ef3566b00f8bd464f0037
OliverDD <QAQddbest@outlook.com>
2020-12-05 11:50:31 +0800

    add id621

---
+++ leetcode/src/main/java/ojij/common/id621/Solution.java
@@ -0,0 +1,18 @@
+package ojij.common.id621;
+
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+/**
+ * 想法是：
+ * 1. 用26长度的数组tasks，记录每个任务的数量
+ * 2. 用26长度的数组pending，记录每个任务最快那个时间点可以运行
+ * 遍历tasks：
+ * time跳到最小的pending值那
+ * 对所有pending相同的任务，找到剩余时间最大的任务，执行并更新pending为n
+ */
+public class Solution {
+    public int leastInterval(char[] tasks, int n) {
+        // TODO: 太太太麻烦了吧...写不动了...
+        throw new NotImplementedException();
+    }
+}
